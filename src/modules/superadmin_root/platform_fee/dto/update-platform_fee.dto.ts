f4ead578c9474004709855f47cbc4df9bc19d34e
Mehedi Hasan Ridoy <khurshed645356@gmail.com>
2025-12-02 04:18:04 +0600

    feat: Implement user-specific fee management
    This commit introduces user-specific fee structures.
    It includes a new DTO, controller, and service.
    Also, corrects a typo in the Raider Deduction Fee controller's API tag.

--- src/modules/superadmin_root/platform_fee/dto/update-platform_fee.dto.ts
+++ src/modules/superadmin_root/platform_fee/dto/update-platform_fee.dto.ts
@@ -2,10 +2,14 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateStandardCommissionRateDto } from './create-commission_rate.dto';
 import { CreateRaiderCompensationRoleDto } from './create_compensation_role.dto';
 import { CreateRaiderDeductionFeeDto } from './create_deduction_fee.dto';
+import { CreateUserFeeStructureDto } from './create_ user_fee_structure.dto';
 
 export class UpdateStandardCommissionRateDto extends PartialType(CreateStandardCommissionRateDto) {}
 // 
 export class UpdateRaiderCompensationRoleDto extends PartialType(CreateRaiderCompensationRoleDto) {}
 // 
 export class UpdateRaiderDeductionFeeDto extends PartialType(CreateRaiderDeductionFeeDto) {}
+// 
+export class UpdateUserFeeStructureDto extends PartialType(CreateUserFeeStructureDto) {}
+
 
--- src/modules/superadmin_root/platform_fee/platform_fee.module.ts
+++ src/modules/superadmin_root/platform_fee/platform_fee.module.ts
@@ -5,9 +5,11 @@ import { RaiderCompensationRoleController } from './compensation_role.controller
 import { RaiderCompensationRoleService } from './compensation_role.service';
 import { RaiderDeductionFeeController } from './deduction_fee.controller';
 import { RaiderDeductionFeeService } from './deduction_fee.service';
+import { UserFeeStructureController } from './fee_structure.controller';
+import { UserFeeStructureService } from './fee_structure.service';
 
 @Module({
-  controllers: [StandardCommissionRateController, RaiderCompensationRoleController, RaiderDeductionFeeController],
-  providers: [StandardCommissionRateService, RaiderCompensationRoleService, RaiderDeductionFeeService],
+  controllers: [StandardCommissionRateController, RaiderCompensationRoleController, RaiderDeductionFeeController, UserFeeStructureController],
+  providers: [StandardCommissionRateService, RaiderCompensationRoleService, RaiderDeductionFeeService , UserFeeStructureService],
 })
 export class PlatformFeeModule {}
