6df0a618c5cb9abff78d067193ddf5ca0791e231
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2025-12-02 06:26:42 +0600

    remove unnecessary sql file

--- prisma/migrations/20251119231612_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "destinations" ALTER COLUMN "order_id" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "orders" ALTER COLUMN "order_status" SET DEFAULT 'PROGRESS';
--- prisma/migrations/20251201213249_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "raider_registrations" ALTER COLUMN "suspendedDuration" DROP NOT NULL,
-ALTER COLUMN "suspensionReason" DROP NOT NULL;
