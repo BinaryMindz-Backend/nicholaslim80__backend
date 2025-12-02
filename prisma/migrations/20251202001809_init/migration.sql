8144494f15632e366905a4704cdc46496a676a38
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2025-12-02 06:24:14 +0600

    ok

--- prisma/migrations/20251202001809_init/migration.sql
+++ prisma/migrations/20251202001809_init/migration.sql
@@ -1,10 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `score` on the `raider_quizzes` table. The data in that column could be lost. The data in that column will be cast from `Decimal(6,2)` to `Integer`.
-  - Made the column `score` on table `raider_quizzes` required. This step will fail if there are existing NULL values in that column.
-
-*/
 -- CreateEnum
 CREATE TYPE "ApplicableTyp" AS ENUM ('RAIDER', 'USER');
 
@@ -15,13 +8,7 @@ CREATE TYPE "FeeAppliesType" AS ENUM ('ALL_ORDERS', 'ORDERLESS15', 'EXPRESS_ORDE
 CREATE TYPE "Condition" AS ENUM ('HIGH_DEMAND', 'VERY_HIGH_DEMAND', 'WEEKEND');
 
 -- AlterTable
-ALTER TABLE "raider_quizzes" ADD COLUMN     "attempt_count" INTEGER NOT NULL DEFAULT 1,
-ALTER COLUMN "total_questions" SET DEFAULT 0,
-ALTER COLUMN "correct_answers" SET DEFAULT 0,
-ALTER COLUMN "score" SET NOT NULL,
-ALTER COLUMN "score" SET DEFAULT 0,
-ALTER COLUMN "score" SET DATA TYPE INTEGER,
-ALTER COLUMN "completed_at" SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "raider_quizzes" ALTER COLUMN "completed_at" SET DEFAULT CURRENT_TIMESTAMP;
 
 -- CreateTable
 CREATE TABLE "StandardCommissionRate" (
