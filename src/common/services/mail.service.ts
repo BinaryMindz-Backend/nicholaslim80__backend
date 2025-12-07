4c5cb3a2eef5aa427511318a52b84ce27d62f108
Mehedi Hasan Ridoy <khurshed645356@gmail.com>
2025-12-08 01:38:52 +0600

    Feat:common-email-services-added

---
+++ src/common/services/mail.service.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+
+@Injectable()
+export class MailService {
+  private transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+  }
+
+  async sendMail({ to, subject, text }: { to: string; subject: string; text: string }) {
+    return await this.transporter.sendMail({ to, subject, text });
+  }
+}
