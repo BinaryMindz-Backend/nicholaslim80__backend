51b8cea00ae0eedf99b6af4d64ac174c4ca0cad2
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2025-12-16 00:42:24 +0600

    Auto commit with changes: D	prisma/migrations/20251214233658_add_poperty_on_order/migration.sql D	prisma/migrations/20251214235450_compitititor_id_added_to_order/migration.sql D	prisma/migrations/20251215003302_add_poperty_on_order/migration.sql D	prisma/migrations/20251215011549_add_rank_and_rating/migration.sql R098	prisma/migrations/20251215170402_new/migration.sql	prisma/migrations/20251215183712_afd/migration.sql

--- prisma/migrations/20251215003302_add_poperty_on_order/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "orders" ADD COLUMN     "competition_closed" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "competition_started_at" TIMESTAMP(3),
-ALTER COLUMN "compititor_id" SET DEFAULT ARRAY[]::INTEGER[];
