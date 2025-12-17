c7c28d5f8c4c821683d44c2b9156de5adebceb9d
Mehedi Hasan Ridoy <khurshed645356@gmail.com>
2025-12-18 04:58:10 +0600

    Guard-added-on-transactions

--- src/modules/superadmin_root/transactions/transactions.controller.ts
+++ src/modules/superadmin_root/transactions/transactions.controller.ts
@@ -3,6 +3,8 @@ import { TransactionsService } from './transactions.service';
 import { Auth } from 'src/decorators/auth.decorator';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ApiResponses } from 'src/common/apiResponse';
+import { RequirePermission } from 'src/rbac/decorators/require-permission.decorator';
+import { Module, Permission } from 'src/rbac/rbac.constants';
 
 @Controller('transactions')
 export class TransactionsController {
@@ -12,7 +14,7 @@ export class TransactionsController {
   @Get()
   @Auth()
   @ApiBearerAuth()
-  // @RequirePermission(Module.LIVE_MAP, Permission.READ)
+  @RequirePermission(Module.PAYMENT_TRANSACTION, Permission.READ)
   @ApiOperation({ summary: 'Get all transaction records' })
   @ApiResponse({ status: 200, description: 'Transactions data fetched successfully' })
   async findAll() {
