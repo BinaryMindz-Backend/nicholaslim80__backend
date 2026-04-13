eb1bd0a2874718b146712c95055e6fc12963217f
Mehedi Hasan Ridoy <khurshed645356@gmail.com>
2026-04-13 22:03:50 +0600

    Update

--- src/modules/superadmin_root/dashboard_popup/dto/update-dashboard_popup.dto.ts
+++ src/modules/superadmin_root/dashboard_popup/dto/update-dashboard_popup.dto.ts
@@ -7,12 +7,17 @@ export class UpdateDashboardPopupDto {
   @IsString()
   title?: string;
 
+  @ApiPropertyOptional( { description: 'Image link for the popup', example: 'https://example.com/popup-image.jpg' })
+  @IsOptional()
+  @IsString()
+  image_link?: string;
+
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   desc?: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional( { description: 'Redirect link for the popup', example: 'https://example.com/redirect' })
   @IsOptional()
   @IsString()
   redirect_link?: string;
