6d777f90975d31624e787e7888ba8eec01b76067
Mehedi Hasan Ridoy <softvence.mehedihasan@gmail.com>
2026-05-25 06:15:02 +0600

    Update:send-notification-from-update

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { ChatController, } from './message.controller';
 import { MessagesGateway } from './message.gateway';
 import { MessagesService } from './message.service';
+import { QueueModule } from '../queue/queue.module';
 
 @Module({
+  imports:[
+      QueueModule
+  ],
   controllers: [ChatController],
   providers: [MessagesService, MessagesGateway],
 })
